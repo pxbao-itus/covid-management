24aa53243480b34a8edfdd3941971e730679ec9d
XuanBao <pxbao.itus@gmail.com>
2021-12-12 17:08:47 +0700

    model and controller for user authentication

--- models/db.js
+++ models/db.js
@@ -12,3 +12,50 @@ const cn = {
     port: process.env.PORT_DATABASE,
 };
 const db = pgp(cn);
+
+
+// exports for account
+exports.loadAccount = async tbName => {
+    const table = new pgp.helpers.TableName({table: tbName, schema: schema});
+    const qStr = pgp.as.format('SELECT * FROM $1', table);
+    try {
+        const res = await db.any(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.getAccount = async (tbName, filedName, value) => {
+    const table = new pgp.helpers.TableName({table: tbName, schema: schema});
+    const qStr = pgp.as.format(`SELECT * FROM $1 WHERE "${filedName}" = '${value}'`, table);
+    try {
+        const res = await db.any(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+exports.addAccount = async (tbName, entity) => {
+    const table = new pgp.helpers.TableName({table: tbName, schema: schema});
+    const qStr = pgp.helpers.insert(entity, null, table) + "RETURNING *";
+    try {
+        const res = await db.one(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.updateAccount = async (tbName, entity, value) => {
+    const table = new pgp.helpers.TableName({table: tbName, schema: schema});
+    const condition = pgp.as.format(' WHERE "Username" = $1', [value]);
+    const qStr = pgp.helpers.update(entity, null, table) + condition + " RETURNING *";
+    console.log(qStr)
+    try {
+        const res = await db.one(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+}
