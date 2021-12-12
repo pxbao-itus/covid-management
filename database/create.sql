b9b1e1268bd467bb370175405933f15100002685
XuanBao <pxbao.itus@gmail.com>
2021-12-12 14:54:48 +0700

    Add script to create database include table, constraint

---
+++ database/create.sql
@@ -0,0 +1,534 @@
+DROP TABLE IF EXISTS LichSuDuocQuanLy;
+DROP TABLE IF EXISTS MoiLienHe;
+DROP TABLE IF EXISTS LichSuThanhToan;
+DROP TABLE IF EXISTS SoDuNo;
+DROP TABLE IF EXISTS TaiKhoanNguoiDung;
+DROP TABLE IF EXISTS ChiTietGoiNYP;
+DROP TABLE IF EXISTS ChiTietMuaGoiNYP;
+DROP TABLE IF EXISTS NhuYeuPham;
+DROP TABLE IF EXISTS LichSuMuaGoiNYP;
+DROP TABLE IF EXISTS GoiNhuYeuPham;
+DROP TABLE IF EXISTS NguoiLienQuan;
+DROP TABLE IF EXISTS NoiDieuTriCachLy;
+DROP TABLE IF EXISTS QuanLyThanhToan;
+DROP TABLE IF EXISTS LichSuNguoiQuanLy;
+DROP TABLE IF EXISTS TaiKhoanNguoiQuanLy;
+DROP TABLE IF EXISTS SoNguoiTungTrangThai;
+DROP TABLE IF EXISTS TaiKhoanThanhToan;
+DROP TABLE IF EXISTS TaiKhoanNguoiDungHTTT;
+DROP TABLE IF EXISTS TaiKhoanHTTT;
+DROP TABLE IF EXISTS Xa;
+DROP TABLE IF EXISTS Huyen;
+DROP TABLE IF EXISTS Tinh;
+
+
+
+-- ----------------------------
+-- Table structure for NguoiLienQuan
+-- ----------------------------
+CREATE TABLE NguoiLienQuan(
+	MaNguoiLienQuan int4 NOT NULL,
+	HoTen varchar(30),
+	CCCD varchar(12),
+	NgaySinh timestamp,
+	DiaChi varchar(50),
+	TrangThaiHienTai varchar(5),
+	NoiDieuTri int4 NOT NULL,
+	
+	PRIMARY KEY (MaNguoiLienQuan)
+);
+
+
+
+-- ----------------------------
+-- Table structure for TaiKhoanNguoiDung
+-- ----------------------------
+
+CREATE TABLE TaiKhoanNguoiDung(
+	NguoiLienQuan int4 NOT NULL,
+	Username varchar(12),
+	Password varchar(50),
+	TrangThai int4,
+	
+	PRIMARY KEY (NguoiLienQuan)
+	
+);
+
+
+
+
+
+-- ----------------------------
+-- Table structure for NoiDieuTriCachLy
+-- ----------------------------
+
+CREATE TABLE NoiDieuTriCachLy(
+	MaNoiDTCL int4 NOT NULL,
+	TenNoiDTCL varchar(30),
+	SucChua int4,
+	SoLuongHienTai int4,
+	DiaChi varchar(50),
+	Loai int4,
+	
+	PRIMARY KEY (MaNoiDTCL)
+	
+);
+
+-- ----------------------------
+-- Table structure for MoiLienHe
+-- ----------------------------
+
+CREATE TABLE MoiLienHe(
+	NguoiLienQuan1 int4 NOT NULL,
+	NguoiLienQuan2 int4 NOT NULL,
+
+	PRIMARY KEY (NguoiLienQuan1,NguoiLienQuan2)
+	
+);
+
+-- ----------------------------
+-- Table structure for LichSuThanhToan
+-- ----------------------------
+
+CREATE TABLE LichSuThanhToan(
+	NguoiLienQuan int4 NOT NULL,
+	ThoiGian timestamp,
+	SoTien numeric(19,4),
+	
+	PRIMARY KEY (NguoiLienQuan,ThoiGian)
+	
+);
+
+-- ----------------------------
+-- Table structure for SoDuNo
+-- ----------------------------
+CREATE TABLE SoDuNo(
+	NguoiLienQuan int4 NOT NULL,
+	SoDuNo int4 NOT NULL,
+	
+	PRIMARY KEY (NguoiLienQuan,SoDuNo)
+	
+);
+
+-- ----------------------------
+-- Table structure for LichSuDuocQuanLy
+-- ----------------------------
+CREATE TABLE LichSuDuocQuanLy(
+	NguoiLienQuan int4 NOT NULL,
+	ThoiGian timestamp NOT NULL,
+	TrangThaiTruoc varchar(5),
+	TrangThaiSau varchar(5),
+	NoiDTCLTruoc int4,
+	NoiDTCLSau int4,
+	
+	PRIMARY KEY (NguoiLienQuan,ThoiGian)
+	
+);
+
+-- ----------------------------
+-- Table structure for LichSuMuaGoiNYP
+-- ----------------------------
+CREATE TABLE LichSuMuaGoiNYP(
+	MaLichSuMua int4 NOT NULL,
+	NguoiLienQuan int4 NOT NULL,
+	GoiNYP int4,
+	SoTien numeric(19,4),
+	ThoiGian timestamp,
+	
+	PRIMARY KEY (MaLichSuMua)
+	
+);
+
+-- ----------------------------
+-- Table structure for GoiNhuYeuPham
+-- ----------------------------
+CREATE TABLE GoiNhuYeuPham(
+	MaGoiNYP int4 NOT NULL,
+	TenGoiNYP varchar(30),
+	NgayLapGoi timestamp,
+	MucGioiHan int4,
+	ThoiGianGioiHan int4,
+	
+	PRIMARY KEY (MaGoiNYP)
+	
+);
+
+-- ----------------------------
+-- Table structure for  ChiTietGoiNYP
+-- ----------------------------
+CREATE TABLE ChiTietGoiNYP(
+	MaChiTietGoiNYP int4 NOT NULL,
+	NhuYeuPham int4 NOT NULL,
+	SoLuong numeric(19,4),
+	SoLuongToiDa numeric(19,4),
+	SoLuongToiThieu numeric(19,4),
+	
+	PRIMARY KEY (MaChiTietGoiNYP)
+	
+);
+
+-- ----------------------------
+-- Table structure for NhuYeuPham
+-- ----------------------------
+CREATE TABLE NhuYeuPham
+(
+	MaNYP int4 NOT NULL,
+	TenNYP varchar(30),
+	HinhAnh1 varchar(50),
+	HinhAnh2 varchar(50),
+	HinhAnh3 varchar(50),
+	HinhAnh4 varchar(50),
+	DonGia int4,
+	DonViDinhLuong varchar(10),
+	
+	PRIMARY KEY (MaNYP)
+	
+);
+
+-- ----------------------------
+-- Table structure for ChiTietMuaGoiNYP
+-- ----------------------------
+CREATE TABLE ChiTietMuaGoiNYP(
+	LichSuMua int4 NOT NULL,
+	NhuYeuPham int4 NOT NULL,
+	SoLuong numeric(19,4),
+	DonGia int4,
+	
+	PRIMARY KEY (LichSuMua,NhuYeuPham)
+	
+);
+
+-- ----------------------------
+-- Table structure for QuanLyThanhToan
+-- ----------------------------
+CREATE TABLE QuanLyThanhToan(
+	MaQLTT int4 NOT NULL,
+	NguoiCapNhat int4 NOT NULL,
+	ThoiGianCapNhat timestamp,
+	HanMuc int4,
+	
+	PRIMARY KEY (MaQLTT)
+	
+);
+
+-- ----------------------------
+-- Table structure for TaiKhoanNguoiQuanLy
+-- ----------------------------	
+CREATE TABLE TaiKhoanNguoiQuanLy(
+	MaTaiKhoan int4 NOT NULL,
+	Password varchar(30),
+	Username varchar(12),
+	TrangThai int4,
+	
+	PRIMARY KEY (MaTaiKhoan)
+	
+);
+
+-- ----------------------------
+-- Table structure for LichSuNguoiQuanLy
+-- ----------------------------
+CREATE TABLE LichSuNguoiQuanLy(
+	MaLichSu int4 NOT NULL,
+	NguoiQuanLy int4 NOT NULL,
+	ThoiGian timestamp,
+	DoiTuong varchar(20),
+	HanhDong varchar(20),
+	MaDong int4,
+	GiaTriTruoc varchar(50),
+	GiaTriSau varchar(50),
+	
+	PRIMARY KEY (MaLichSu)
+	
+);
+
+-- ----------------------------
+-- Table structure for SoNguoiTungTrangThai
+-- ----------------------------
+
+CREATE TABLE SoNguoiTungTrangThai(
+	ThoiGian timestamp,
+	F0 int4,
+	F1 int4,
+	F2 int4,
+	F3 int4,
+	KhoiBenh int4,
+	BinhThuong int4,
+	TuVong int4,
+	
+	PRIMARY KEY (ThoiGian)
+	
+);
+
+-- ----------------------------
+-- Table structure for TaiKhoanNguoiDungHTTT
+-- ----------------------------	
+CREATE TABLE TaiKhoanNguoiDungHTTT(
+	MaTaiKhoan int4 NOT NULL,
+	Username varchar(20),
+	Password varchar(50),
+	
+	PRIMARY KEY(MaTaiKhoan)
+);
+
+-- ----------------------------
+-- Table structure for TaiKhoanThanhToan
+-- ----------------------------	
+
+CREATE TABLE TaiKhoanThanhToan(
+	MaTaiKhoan int4 NOT NULL,
+	SoDu numeric(19,4),
+	
+	PRIMARY KEY(MaTaiKhoan)
+);
+
+-- ----------------------------
+-- Table structure for TaiKhoanHTTT
+-- ----------------------------	
+
+
+CREATE TABLE TaiKhoanHTTT(
+	SoDu numeric(19,4)
+);
+
+-- ----------------------------
+-- Table structure for Tinh
+-- ----------------------------	
+
+CREATE TABLE Tinh(
+	MaTinh int4 NOT NULL,
+	TenTinh varchar(20),
+	
+	PRIMARY KEY(MaTinh)
+	
+);
+
+-- ----------------------------
+-- Table structure for Huyen
+-- ----------------------------	
+
+CREATE TABLE Huyen(
+	MaHuyen int4 NOT NULL,
+	TenHuyen varchar(20),
+	Tinh int4 NOt NULL,
+	
+	PRIMARY KEY(MaHuyen)
+	
+);
+
+-- ----------------------------
+-- Table structure for Xa
+-- ----------------------------	
+
+CREATE TABLE Xa(
+	MaXa int4 NOT NULL,
+	TenXa varchar(20),
+	Huyen int4 NOt NULL,
+	
+	PRIMARY KEY(MaXa)
+	
+);
+
+----------------------------------------------------------------------------
+-----------------------
+---- NguoiLienQuan ---> NoiDTCL--
+-----------------------
+ALTER TABLE NguoiLienQuan
+DROP CONSTRAINT IF EXISTS FK_NguoiLienQuan_NoiCachLyDieuTri;
+
+ALTER TABLE NguoiLienQuan 
+ADD CONSTRAINT FK_NguoiLienQuan_NoiCachLyDieuTri
+FOREIGN KEY (NoiDieuTri)
+REFERENCES NoiDieuTriCachLy(MaNoiDTCL);
+
+-----------------------
+---- TaiKhoanNguoiDung ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE TaiKhoanNguoiDung
+DROP CONSTRAINT IF EXISTS FK_TaiKhoanNguoiDung_NguoiLienQuan;
+
+ALTER TABLE TaiKhoanNguoiDung 
+ADD CONSTRAINT FK_TaiKhoanNguoiDung_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+-----------------------
+---- MoiLienHe ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE MoiLienHe
+DROP CONSTRAINT IF EXISTS FK_MoiLienHe1_NguoiLienQuan;
+
+ALTER TABLE MoiLienHe 
+ADD CONSTRAINT FK_MoiLienHe1_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan1)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+
+ALTER TABLE MoiLienHe
+DROP CONSTRAINT IF EXISTS FK_MoiLienHe2_NguoiLienQuan;
+
+ALTER TABLE MoiLienHe 
+ADD CONSTRAINT FK_MoiLienHe2_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan2)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+
+-----------------------
+---- LichSuThanhToan ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE LichSuThanhToan
+DROP CONSTRAINT IF EXISTS FK_LichSuThanhToan_NguoiLienQuan;
+
+ALTER TABLE LichSuThanhToan 
+ADD CONSTRAINT FK_LichSuThanhToan_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+-----------------------
+---- SoDuNo ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE SoDuNo
+DROP CONSTRAINT IF EXISTS FK_SoDuNo_NguoiLienQuan;
+
+ALTER TABLE SoDuNo 
+ADD CONSTRAINT FK_SoDuNo_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+-----------------------
+---- LichSuDuocQuanLy ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE LichSuDuocQuanLy
+DROP CONSTRAINT IF EXISTS FK_LichSuDuocQuanLy_NguoiLienQuan;
+
+ALTER TABLE LichSuDuocQuanLy 
+ADD CONSTRAINT FK_LichSuDuocQuanLy_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+-----------------------
+---- LichSuMuaGoiNYP ---> NguoiLienQuan--
+-----------------------
+
+ALTER TABLE LichSuMuaGoiNYP
+DROP CONSTRAINT IF EXISTS FK_LichSuMuaGoiNYP_NguoiLienQuan;
+
+ALTER TABLE LichSuMuaGoiNYP 
+ADD CONSTRAINT FK_LichSuMuaGoiNYP_NguoiLienQuan
+FOREIGN KEY (NguoiLienQuan)
+REFERENCES NguoiLienQuan(MaNguoiLienQuan);
+
+-----------------------
+---- LichSuMuaGoiNYP ---> GoiNhuYeuPham--
+-----------------------
+
+ALTER TABLE LichSuMuaGoiNYP
+DROP CONSTRAINT IF EXISTS FK_LichSuMuaGoiNYP_GoiNhuYeuPham;
+
+ALTER TABLE LichSuMuaGoiNYP 
+ADD CONSTRAINT FK_LichSuMuaGoiNYP_GoiNhuYeuPham
+FOREIGN KEY (GoiNYP)
+REFERENCES GoiNhuYeuPham(MaGoiNYP);
+
+
+-----------------------
+---- ChiTietGoiNYP ---> GoiNhuYeuPham--
+-----------------------
+
+ALTER TABLE ChiTietGoiNYP
+DROP CONSTRAINT IF EXISTS FK_ChiTietGoiNYP_NhuYeuPham;
+
+ALTER TABLE ChiTietGoiNYP 
+ADD CONSTRAINT FK_ChiTietGoiNYP_NhuYeuPham
+FOREIGN KEY (NhuYeuPham)
+REFERENCES NhuYeuPham(MaNYP);
+
+-----------------------
+---- ChiTietMuaGoiNYP ---> LichSuMuaGoiNYP--
+-----------------------
+
+ALTER TABLE ChiTietMuaGoiNYP
+DROP CONSTRAINT IF EXISTS FK_ChiTietMuaGoiNYP_LichSuMuaGoiNYP;
+
+ALTER TABLE ChiTietMuaGoiNYP 
+ADD CONSTRAINT FK_ChiTietMuaGoiNYP_LichSuMuaGoiNYP
+FOREIGN KEY (LichSuMua)
+REFERENCES LichSuMuaGoiNYP(MaLichSuMua);
+
+-----------------------
+---- ChiTietMuaGoiNYP ---> NhuYeuPham--
+-----------------------
+
+ALTER TABLE ChiTietMuaGoiNYP
+DROP CONSTRAINT IF EXISTS FK_ChiTietMuaGoiNYP_NhuYeuPham;
+
+ALTER TABLE ChiTietMuaGoiNYP 
+ADD CONSTRAINT FK_ChiTietMuaGoiNYP_NhuYeuPham
+FOREIGN KEY (NhuYeuPham)
+REFERENCES NhuYeuPham(MaNYP);
+
+-----------------------
+---- QuanLyThanhToan ---> TaiKhoanNguoiQuanLy--
+-----------------------
+
+ALTER TABLE QuanLyThanhToan
+DROP CONSTRAINT IF EXISTS FK_QuanLyThanhToan_TaiKhoanNguoiQuanLy;
+
+ALTER TABLE QuanLyThanhToan 
+ADD CONSTRAINT FK_QuanLyThanhToan_TaiKhoanNguoiQuanLy
+FOREIGN KEY (NguoiCapNhat)
+REFERENCES TaiKhoanNguoiQuanLy(MaTaiKhoan);
+
+
+-----------------------
+---- LichSuNguoiQuanLy ---> TaiKhoanNguoiQuanLy--
+-----------------------
+
+ALTER TABLE LichSuNguoiQuanLy
+DROP CONSTRAINT IF EXISTS FK_LichSuNguoiQuanLy_TaiKhoanNguoiQuanLy;
+
+ALTER TABLE LichSuNguoiQuanLy 
+ADD CONSTRAINT FK_LichSuNguoiQuanLy_TaiKhoanNguoiQuanLy
+FOREIGN KEY (NguoiQuanLy)
+REFERENCES TaiKhoanNguoiQuanLy(MaTaiKhoan);
+
+
+-----------------------
+---- TaiKhoanThanhToan ---> TaiKhoanNguoiDungHTTT--
+-----------------------
+
+ALTER TABLE TaiKhoanThanhToan
+DROP CONSTRAINT IF EXISTS FK_TaiKhoanThanhToan_TaiKhoanNguoiDungHTTT;
+
+ALTER TABLE TaiKhoanThanhToan 
+ADD CONSTRAINT FK_TaiKhoanThanhToan_TaiKhoanNguoiDungHTTT
+FOREIGN KEY (MaTaiKhoan)
+REFERENCES TaiKhoanNguoiDungHTTT(MaTaiKhoan);
+
+-----------------------
+---- Huyen ---> Tinh --
+-----------------------
+
+ALTER TABLE Huyen
+DROP CONSTRAINT IF EXISTS FK_Huyen_Tinh;
+
+ALTER TABLE Huyen 
+ADD CONSTRAINT FK_Huyen_Tinh
+FOREIGN KEY (Tinh)
+REFERENCES Tinh(MaTinh);
+
+-----------------------
+---- Xa ---> Huyen --
+-----------------------
+
+ALTER TABLE Xa
+DROP CONSTRAINT IF EXISTS FK_Xa_Huyen;
+
+ALTER TABLE Xa 
+ADD CONSTRAINT FK_Xa_Huyen
+FOREIGN KEY (Huyen)
+REFERENCES Huyen(MaHuyen);
