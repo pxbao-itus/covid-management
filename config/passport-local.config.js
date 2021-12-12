64703c83ea047231685f5c928e2d6b96183905e3
XuanBao <pxbao.itus@gmail.com>
2021-12-12 17:08:07 +0700

    config local passport for authenticate

---
+++ config/passport-local.config.js
@@ -0,0 +1,49 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+const accountModel = require('../models/account.model');
+
+
+module.exports = app => {
+    passport.use(new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+    },
+        async (username, password, done) => {
+            let account;
+            var tableName;
+            try {
+                if(username.indexOf('mng') >= 0) {
+                    tableName = 'TaiKhoanNguoiQuanLy';
+                } else {
+                    tableName = 'TaiKhoanNguoiDung';
+                }
+                account = await accountModel.get(tableName, username);
+                if(!account) {
+                    return done(null, false, {message: 'Incorrect username.'});
+                }
+                const challengeResult = await bcrypt.compare(password, user.Password);
+                if(!challengeResult) {
+                    return done(null, false, {message: 'Incorrect password.'});
+                }
+                return done(null, account);
+            } catch (error) {
+                return done(error);
+            }
+        }
+    ));
+    passport.serializeUser(async (account, done) => {
+        return done(null, account);
+    })
+    passport.deserializeUser(async (account, done) => {
+        try {
+            account = await accountModel.get(tableName, account.Username);
+            return done(null, account);
+        } catch (error) {
+            return done(new Error('error'), null);
+        }
+    })
+    app.use(passport.initialize());
+    app.use(passport.session());
+}
