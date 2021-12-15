4358b2c1daccef9fb26d8c1cd6afdc151bbfd45a
XuanBao <pxbao.itus@gmail.com>
2021-12-15 17:41:01 +0700

    backend router for  admin manage manager

---
+++ models/admin/manager.model.js
@@ -0,0 +1,24 @@
+const db = require('../db')
+
+module.exports = {
+    create: async (entity) => {
+        const res = await db.createManagerAccount(entity);
+        return res;
+    },
+    list: async () => {
+        const res = await db.listManagerAccount();
+        return res;
+    },
+    history: async (value) => {
+        const res = await db.historyManagerAction(value);
+        return res;
+    },
+    delete: async (value) => {
+        const res = await db.deleteManagerAccount(value);
+        return res;
+    },
+    update: async (entity, value) => {
+        const res = await db.updateManagerAccount(entity, value);
+        return res;
+    }
+}
--- models/db.js
+++ models/db.js
@@ -19,8 +19,8 @@ const db = pgp(cn);
 const NYP = 'NhuyeuPham';
 const GoiNYP = 'GoiNhuYeuPham';
 const ChiTietGoiNYP = 'ChiTietGoiNYP';
-
-
+const TaiKhoanNguoiQuanLy = 'TaiKhoanNguoiQuanLy';
+const LichSuNguoiQuanLy = 'LichSuNguoiQuanLy';
 
 // exports for account
 exports.loadAccount = async tbName => {
@@ -220,3 +220,69 @@ exports.createPackage = async (package, details) => {
         return false;
     }
 }
+
+
+// exports for admin: manage manager
+exports.createManagerAccount = async (entity) => {
+    const table = new pgp.helpers.TableName({table: TaiKhoanNguoiQuanLy, schema: schema}); 
+    const qStr = pgp.helpers.insert(entity, null, table) + "RETURNING *";
+    try {
+        const res = await db.one(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+exports.listManagerAccount = async () => {
+    const table = new pgp.helpers.TableName({table: TaiKhoanNguoiQuanLy, schema: schema});
+    const qStr = pgp.as.format('SELECT * FROM $1', table);
+    try {
+        const res = await db.any(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+exports.updateManagerAccount = async (entity, value) => {
+    const table = new pgp.helpers.TableName({table: TaiKhoanNguoiQuanLy, schema: schema});
+    const condition = pgp.as.format(' WHERE "MaTaiKhoan" = $1', [value]);
+    const qStr = pgp.helpers.update(entity, null, table) + condition + " RETURNING *";
+    console.log(qStr)
+    try {
+        const res = await db.one(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+exports.deleteManagerAccount = async (value) => {
+    const manager = new pgp.helpers.TableName({table: TaiKhoanNguoiQuanLy, schema: schema});
+    const history = new pgp.helpers.TableName({table: LichSuNguoiQuanLy, schema: schema});
+    const queryManager = pgp.as.format('DELETE FROM $1 WHERE "MaTaiKhoan" = $2', [manager, value]);
+    const queryHistory = pgp.as.format('DELETE FROM $1 WHERE "NguoiQuanLy" = $2', [history, value]);
+    try {
+        await pgp.none(queryHistory);
+        await pgp.none(queryManager);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+exports.historyManagerAction = async (value) => {
+    const table = new pgp.helpers.TableName({table: LichSuNguoiQuanLy, schema: schema});
+    const qStr = pgp.as.format(`SELECT * FROM $1 WHERE "NguoiQuanLy" = $2`, [table, value]);
+    try {
+        const res = await db.any(qStr);
+        return res;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
