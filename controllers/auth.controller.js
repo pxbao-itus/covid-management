27849b3fa7e7ec3b87aab3ce0980c0d5ae9165f9
XuanBao <pxbao.itus@gmail.com>
2021-12-15 18:55:10 +0700

    backend router for change password and fix sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -12,25 +12,28 @@ authUser.get('/signin',async (req, res) =>{
     if(!await accountModel.all(adminAccountTable)) {
         return res.redirect('/init');
     }
-    if(req.signedCookies.user) {
-        let user = await userModel.get(userAccountTable, req.signedCookies.user);
-        if(user) {
-            return res.redirect('/home');
-        } else {
-            user = await userModel.get(managerAccountTable, req.signedCookies.user);
+    if(req.keep-signin === 'true') {
+        if(req.signedCookies.user) {
+            let user = await userModel.get(userAccountTable, req.signedCookies.user);
             if(user) {
-                return res.redirect('/manager', {
-                    manager: user.Username
-                })
+                return res.redirect('/home');
             } else {
-                user = await userModel.get(adminAccountTable, req.signedCookies.user); 
+                user = await userModel.get(managerAccountTable, req.signedCookies.user);
                 if(user) {
-                    return res.redirect('/admin');
+                    return res.redirect('/manager', {
+                        manager: user.Username
+                    })
                 } else {
-                    return res.render('signin');
+                    user = await userModel.get(adminAccountTable, req.signedCookies.user); 
+                    if(user) {
+                        return res.redirect('/admin');
+                    } else {
+                        return res.render('signin');
+                    }
                 }
             }
         }
+        
     }
     if(req.account) {
         let user = await userModel.get(userAccountTable, req.account.Username);
@@ -74,8 +77,11 @@ authUser.post('/signin',async (req, res, next) => {
                 });
             }
             if(req.body.keep === 'on') {
-                res.cookie('user', user.Username, {signed: true});
+                res.cookie('keep-signin', 'true');
+            } else {
+                res.cookie('keep-signin', 'false');
             }
+            res.cookie('user', user.Username, {signed: true});
             let account = await accountModel.get(userAccountTable, user.Username);
             if(account) {
                 const challengeResult = await bcrypt.compare(password, account.Password);
@@ -123,6 +129,7 @@ authUser.post('/signin',async (req, res, next) => {
 
 
 authUser.get('/signout', (req, res) => {
+    
     if(req.signedCookies.user){
         res.clearCookie('user');
     }
