07a32357e8f99043a9eb94923467034524fb8df1
Hiendang7613 <73353492+hiendang7613@users.noreply.github.com>
2021-12-18 13:37:36 +0700

    init manger product and package

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT
 // config app
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser(process.env.SECRET_KEY));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', '.hbs');
