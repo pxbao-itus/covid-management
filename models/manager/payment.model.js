a7fd769bd1e745bd0ce012f11541b991b06335d9
= <leanhtuanlhln@gmail.com>
2021-12-21 15:10:00 +0700

    complete backend feature payment

--- models/manager/payment.model.js
+++ models/manager/payment.model.js
@@ -1,19 +1,38 @@
 const db = require('../payment');
 
-const tbName = 'TaiKhoanNguoiQuanLy';
-
 module.exports = {
     update: async (entity) => {
-        const res = await db.updateLimit(entity, tbName);
+        const res = await db.updateLimit(entity);
         return res;
-    }
-}
+    },
 
-module.exports = {
     accept: async (arrayId) => {
         const res = await db.acceptPayment(arrayId);
         return res;
-    }
-}
+    },
 
+    list: async (value) => {
+        const res = await db.loadHistory(value);
+        return res;
+    },
+    announce: async (value) => {
+        const res = await db.announcePayment(value);
+        return res;
+    },
+
+    create: async (entity, value) => {
+        const res = await db.createPayment(entity, value);
+        return res;
+    },
+
+    get: async (value) => {
+        const res = await db.getAccount(value);
+        return res;
+    },
+
+    recharge: async (entity, value) => {
+        const res = await db.rechargeMoney(entity, value);
+        return res;
+    },
+};
 
