500d40972d1273b181402fca3fdb7de96d63cdad
anhtoan1103 <you@example.com>
2021-12-25 13:39:55 +0700

    admin init

--- controllers/admin/manager.controller.js
+++ controllers/admin/manager.controller.js
@@ -3,14 +3,14 @@ const managerModel = require('../../models/admin/manager.model');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-managerRouter.get('/list', async (req, res) => {
+managerRouter.get('/list', async(req, res) => {
     let result;
     try {
         result = await managerModel.list();
     } catch (error) {
         result = [];
     }
-    return res.render('admin/mamagerList', {
+    return res.render('admin/managerList', {
         managers: result
     })
 })
@@ -20,7 +20,7 @@ managerRouter.get('/create', (req, res) => {
 })
 
 
-managerRouter.post('/create', async (req, res) => {
+managerRouter.post('/create', async(req, res) => {
 
     try {
         const Password = await bcrypt.hash(req.body.password, saltRounds);
@@ -31,31 +31,31 @@ managerRouter.post('/create', async (req, res) => {
         }
         const result = await managerModel.create(entity);
     } catch (error) {
-        
+
     }
     return res.redirect('/admin/manager/create');
 })
 
-managerRouter.get('/disable', async (req, res) => {
+managerRouter.get('/disable', async(req, res) => {
     try {
         const entity = {
             TrangThai: req.query.status
         }
         const result = managerModel.update(entity, req.query.id);
     } catch (error) {
-        
+
     }
     return res.redirect('/admin/manager/list');
 })
-managerRouter.get('/delete?', async (req, res) => {
+managerRouter.get('/delete?', async(req, res) => {
     try {
         const result = managerModel.delete(req.query.id);
     } catch (error) {
-        
+
     }
     return res.redirect('/admin/manager/list');
 })
-managerRouter.get('/history', async (req, res) => {
+managerRouter.get('/history', async(req, res) => {
     let result;
     try {
         result = await managerModel.history(req.query.id);
