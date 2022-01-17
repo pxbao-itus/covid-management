d563d09e4f8a72b3f77ae76289fd3c82679361db
XuanBao <pxbao.itus@gmail.com>
2022-01-17 23:39:33 +0700

    Layout for admin

---
+++ public/js/admin/sidebar.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+})
