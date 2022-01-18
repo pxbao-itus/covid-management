3feda0109101469f1f215c8783b9a832ebfeee84
XuanBao <pxbao.itus@gmail.com>
2022-01-18 16:52:30 +0700

    Admin list manager and history

---
+++ public/js/admin/manager.js
@@ -0,0 +1,35 @@
+
+
+async function change(id) {
+  if($(`#${id}`).text() === 'Mở') {
+    const result = await $.get(`/admin/manager/update?id=${id}&status=1`);
+    if(result === 'success') {
+      $(`#${id}`).removeClass('btn-success');
+      $(`#${id}`).addClass('btn-danger');
+      $(`#${id}`).text('Khóa');
+      $(`#s-${id}`).text('Mở');
+      alert('xyz');
+    } else {
+      alert('Mở khóa thất bại!');
+    }
+
+  } else {
+      const result = await $.get(`/admin/manager/update?id=${id}&status=0`);
+      if(result === 'success') {
+        $(`#${id}`).removeClass('btn-danger');
+        $(`#${id}`).addClass('btn-success');
+        $(`#${id}`).text('Mở');
+        $(`#s-${id}`).text('Khóa');
+        alert('abc');
+      } else {
+        alert('Khóa không thành công!');
+      }
+  }
+}
+$(document).ready(function(){
+  $('.change').each(function() {
+      $(this).click(async function(){
+        change($(this).attr('id'));
+  })
+  })
+})
