76c60d0b56485124bba49a3a0194ac64367a7762
XuanBao <pxbao.itus@gmail.com>
2022-01-18 09:23:31 +0700

    Admin: create manager account manual and import file csv

--- controllers/admin/manager.controller.js
+++ controllers/admin/manager.controller.js
@@ -2,6 +2,10 @@ const managerRouter = require('express').Router();
 const managerModel = require('../../models/admin/manager.model');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const upload = require('../../config/multer.config');
+const fs = require('fs');
+const csv = require('csv-parser');
+
 
 managerRouter.get('/list', async(req, res) => {
     let result;
@@ -18,13 +22,49 @@ managerRouter.get('/list', async(req, res) => {
 })
 
 managerRouter.get('/create', (req, res) => {
-    return res.render('admin/managerCreate');
+    return res.render('admin/managerCreate', {
+        layout: 'adminSidebar',
+        title: 'Tạo tài khoản người quản lý',
+        path: 'create'
+    });
 })
 
 
-managerRouter.post('/create', async(req, res) => {
-
+managerRouter.post('/create',upload.single('file'), async(req, res) => {
     try {
+        if(req.file) {
+            fs.createReadStream(req.file.path)
+                .pipe(csv())
+                .on('data', async function(data){
+                    try {
+                        console.log(data);
+                        const Password = await bcrypt.hash(data.password, saltRounds);
+                        const entity = {
+                            Username: data.username,
+                            Password: Password,
+                            TrangThai: 1
+                        }
+                        const result = await managerModel.create(entity);
+                    }
+                    catch(err) {
+                        return res.render('admin/managerCreate', {
+                            layout: 'adminSidebar',
+                            title: 'Tạo tài khoản người quản lý',
+                            path: 'create',
+                            msg: 'Tạo tài khoản không thành công',
+                        })
+                    }
+                })
+                .on('end',function(){                   
+                });
+            return res.render('admin/managerCreate', {
+                layout: 'adminSidebar',
+                title: 'Tạo tài khoản người quản lý',
+                path: 'create',
+                msg: 'Tạo tài khoản thành công',
+                status: 1
+            })
+        }
         const Password = await bcrypt.hash(req.body.password, saltRounds);
         const entity = {
             Username: req.body.username,
@@ -32,10 +72,31 @@ managerRouter.post('/create', async(req, res) => {
             TrangThai: 1
         }
         const result = await managerModel.create(entity);
+        if(result) {
+            return res.render('admin/managerCreate', {
+                layout: 'adminSidebar',
+                title: 'Tạo tài khoản người quản lý',
+                path: 'create',
+                msg: 'Tạo tài khoản thành công',
+                status: 1
+            })
+        } else {
+            return res.render('admin/managerCreate', {
+                layout: 'adminSidebar',
+                title: 'Tạo tài khoản người quản lý',
+                path: 'create',
+                msg: 'Tạo tài khoản không thành công',
+            })
+        }
     } catch (error) {
-
+        console.log(error)
+        return res.render('admin/managerCreate', {
+            layout: 'adminSidebar',
+            title: 'Tạo tài khoản người quản lý',
+            path: 'create',
+            msg: 'Tạo tài khoản không thành công',
+        })
     }
-    return res.redirect('/admin/manager/create');
 })
 
 managerRouter.get('/update', async (req, res) => {
--- public/js/admin/sidebar.js
+++ public/js/admin/sidebar.js
@@ -16,4 +16,14 @@ $(document).ready(function() {
 	$('#sidebarCollapse').on('click', function () {
       $('#sidebar').toggleClass('active');
   });
+
+	if(window.location.href.indexOf('manager/create') >= 0) {
+		$('#create').addClass('active');
+	}
+	if(window.location.href.indexOf('manager/list') >= 0 || window.location.href.indexOf('manager/history') >= 0) {
+		$('#manager').addClass('active');
+	}
+	if(window.location.href.indexOf('treatment') >= 0) {
+		$('#treatment').addClass('active');
+	}
 })
