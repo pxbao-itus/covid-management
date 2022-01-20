2029a52ccf6d1edf49f8347ff16e4426feb24751
= <leanhtuanlhln@gmail.com>
2022-01-20 21:00:53 +0700

    createAccount once add new user

--- controllers/manager/user.controller.js
+++ controllers/manager/user.controller.js
@@ -4,284 +4,313 @@ const userModel = require("../../models/manager/user.model");
 const upload = require('../../config/multer.config');
 const fs = require('fs');
 const csv = require('csv-parser');
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
-user.get("/list", async(req, res) => {
-    var users = await userModel.list();
+user.get("/list", async (req, res) => {
+  var users = await userModel.list();
 
-    const userStatus = {
-        KhoiBenh: 0,
-        BinhThuong: 1,
-        F3: 2,
-        F2: 3,
-        F1: 4,
-        F0: 5,
-        TuVong: 6,
-    };
-    const PAGE_SIZE = 6;
-    const data = [];
-    const resultPagination = {};
-    var currentPage = 1;
-    var totalPage = 1;
+  const userStatus = {
+    KhoiBenh: 0,
+    BinhThuong: 1,
+    F3: 2,
+    F2: 3,
+    F1: 4,
+    F0: 5,
+    TuVong: 6,
+  };
+  const PAGE_SIZE = 6;
+  const data = [];
+  const resultPagination = {};
+  var currentPage = 1;
+  var totalPage = 1;
 
-    users.forEach((element) => {
-        element.Tuoi = _calculateAge(element.NgaySinh);
+  users.forEach((element) => {
+    element.Tuoi = _calculateAge(element.NgaySinh);
+  });
+  if (req.query.search) {
+    users = users.filter((item) => {
+      return (
+        item.HoTen.toLowerCase().indexOf(req.query.search.toLowerCase()) >= 0
+      );
     });
-    if (req.query.search) {
-        users = users.filter((item) => {
-            return (
-                item.HoTen.toLowerCase().indexOf(req.query.search.toLowerCase()) >= 0
-            );
-        });
-        return users;
-    }
+    return users;
+  }
 
-    if (req.query.sort) {
-        const sort = req.query.sort;
-        if (sort === "age") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    item1.Tuoi - item2.Tuoi :
-                    item2.Tuoi - item1.Tuoi;
-            });
-        } else if (sort === "status") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    userStatus[item1.TrangThaiHienTai] -
-                    userStatus[item2.TrangThaiHienTai] :
-                    userStatus[item2.TrangThaiHienTai] -
-                    userStatus[item1.TrangThaiHienTai];
-            });
-        } else if (sort === "id") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    item1.MaNguoiLienQuan - item2.MaNguoiLienQuan :
-                    item2.MaNguoiLienQuan - item1.MaNguoiLienQuan;
-            });
-        }
+  if (req.query.sort) {
+    const sort = req.query.sort;
+    if (sort === "age") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? item1.Tuoi - item2.Tuoi
+          : item2.Tuoi - item1.Tuoi;
+      });
+    } else if (sort === "status") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? userStatus[item1.TrangThaiHienTai] -
+              userStatus[item2.TrangThaiHienTai]
+          : userStatus[item2.TrangThaiHienTai] -
+              userStatus[item1.TrangThaiHienTai];
+      });
+    } else if (sort === "id") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? item1.MaNguoiLienQuan - item2.MaNguoiLienQuan
+          : item2.MaNguoiLienQuan - item1.MaNguoiLienQuan;
+      });
     }
+  }
 
-    if (users) {
-        totalPage = Math.ceil(users.length / PAGE_SIZE);
-    } else {
-        msg = "Không tìm thấy dữ liệu";
-        res.render("manager/user/list", {
-            msg: msg,
-        });
-    }
-
-    if (req.query.page) {
-        currentPage = req.query.page;
-        for (
-            let index = 6 * (req.query.page - 1); index < 6 * req.query.page; index++
-        ) {
-            if (users[index]) {
-                data.push(users[index]);
-            } else {
-                break;
-            }
-        }
-    } else {
-        for (let index = 0; index < 6; index++) {
-            if (users[index]) {
-                data.push(users[index]);
-            } else {
-                break;
-            }
-        }
-    }
-
-    resultPagination.totalPage = totalPage;
-    resultPagination.currentPage = currentPage;
-    resultPagination.data = data;
-    return res.render("manager/user/list", {
-        user: resultPagination.data,
-        total: totalPage,
-        currentPage: currentPage,
-        path: "/manager/user/list",
+  if (users) {
+    totalPage = Math.ceil(users.length / PAGE_SIZE);
+  } else {
+    msg = "Không tìm thấy dữ liệu";
+    res.render("manager/user/list", {
+      msg: msg,
     });
+  }
+
+  if (req.query.page) {
+    currentPage = req.query.page;
+    for (
+      let index = 6 * (req.query.page - 1);
+      index < 6 * req.query.page;
+      index++
+    ) {
+      if (users[index]) {
+        data.push(users[index]);
+      } else {
+        break;
+      }
+    }
+  } else {
+    for (let index = 0; index < 6; index++) {
+      if (users[index]) {
+        data.push(users[index]);
+      } else {
+        break;
+      }
+    }
+  }
+
+  resultPagination.totalPage = totalPage;
+  resultPagination.currentPage = currentPage;
+  resultPagination.data = data;
+  return res.render("manager/user/list", {
+    user: resultPagination.data,
+    total: totalPage,
+    currentPage: currentPage,
+    path: "/manager/user/list",
+  });
 });
-user.post("/list", async(req, res) => {
-    var users = await userModel.list();
+user.post("/list", async (req, res) => {
+  var users = await userModel.list();
 
-    const userStatus = {
-        KhoiBenh: 0,
-        BinhThuong: 1,
-        F3: 2,
-        F2: 3,
-        F1: 4,
-        F0: 5,
-        TuVong: 6,
-    };
-    const PAGE_SIZE = 6;
-    const data = [];
-    const resultPagination = {};
-    var currentPage = 1;
-    var totalPage = 1;
+  const userStatus = {
+    KhoiBenh: 0,
+    BinhThuong: 1,
+    F3: 2,
+    F2: 3,
+    F1: 4,
+    F0: 5,
+    TuVong: 6,
+  };
+  const PAGE_SIZE = 6;
+  const data = [];
+  const resultPagination = {};
+  var currentPage = 1;
+  var totalPage = 1;
 
-    users.forEach((element) => {
-        element.Tuoi = _calculateAge(element.NgaySinh);
+  users.forEach((element) => {
+    element.Tuoi = _calculateAge(element.NgaySinh);
+  });
+  if (req.query.search) {
+    users = users.filter((item) => {
+      return (
+        item.HoTen.toLowerCase().indexOf(req.query.search.toLowerCase()) >= 0
+      );
     });
-    if (req.query.search) {
-        users = users.filter((item) => {
-            return (
-                item.HoTen.toLowerCase().indexOf(req.query.search.toLowerCase()) >= 0
-            );
-        });
-    }
+  }
 
-    if (req.query.sort) {
-        const sort = req.query.sort;
-        if (sort === "age") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    item1.Tuoi - item2.Tuoi :
-                    item2.Tuoi - item1.Tuoi;
-            });
-        } else if (sort === "status") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    userStatus[item1.TrangThaiHienTai] -
-                    userStatus[item2.TrangThaiHienTai] :
-                    userStatus[item2.TrangThaiHienTai] -
-                    userStatus[item1.TrangThaiHienTai];
-            });
-        } else if (sort === "id") {
-            users = users.sort((item1, item2) => {
-                return req.query.order === "increase" ?
-                    item1.MaNguoiLienQuan - item2.MaNguoiLienQuan :
-                    item2.MaNguoiLienQuan - item1.MaNguoiLienQuan;
-            });
-        }
+  if (req.query.sort) {
+    const sort = req.query.sort;
+    if (sort === "age") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? item1.Tuoi - item2.Tuoi
+          : item2.Tuoi - item1.Tuoi;
+      });
+    } else if (sort === "status") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? userStatus[item1.TrangThaiHienTai] -
+              userStatus[item2.TrangThaiHienTai]
+          : userStatus[item2.TrangThaiHienTai] -
+              userStatus[item1.TrangThaiHienTai];
+      });
+    } else if (sort === "id") {
+      users = users.sort((item1, item2) => {
+        return req.query.order === "increase"
+          ? item1.MaNguoiLienQuan - item2.MaNguoiLienQuan
+          : item2.MaNguoiLienQuan - item1.MaNguoiLienQuan;
+      });
     }
+  }
 
-    if (users) {
-        totalPage = Math.ceil(users.length / PAGE_SIZE);
-    } else {
-        msg = "Không tìm thấy dữ liệu";
-        res.render("manager/user/list", {
-            msg: msg,
-        });
-    }
-
-    if (req.query.page) {
-        currentPage = req.query.page;
-        for (
-            let index = 6 * (req.query.page - 1); index < 6 * req.query.page; index++
-        ) {
-            if (users[index]) {
-                data.push(users[index]);
-            } else {
-                break;
-            }
-        }
-    } else {
-        for (let index = 0; index < 6; index++) {
-            if (users[index]) {
-                data.push(users[index]);
-            } else {
-                break;
-            }
-        }
-    }
-
-    resultPagination.totalPage = totalPage;
-    resultPagination.currentPage = currentPage;
-    resultPagination.data = data;
-    res.send({
-        totalPage: resultPagination.totalPage,
-        resultPagnition: resultPagination.data,
-        currentPage: currentPage,
+  if (users) {
+    totalPage = Math.ceil(users.length / PAGE_SIZE);
+  } else {
+    msg = "Không tìm thấy dữ liệu";
+    res.render("manager/user/list", {
+      msg: msg,
     });
+  }
+
+  if (req.query.page) {
+    currentPage = req.query.page;
+    for (
+      let index = 6 * (req.query.page - 1);
+      index < 6 * req.query.page;
+      index++
+    ) {
+      if (users[index]) {
+        data.push(users[index]);
+      } else {
+        break;
+      }
+    }
+  } else {
+    for (let index = 0; index < 6; index++) {
+      if (users[index]) {
+        data.push(users[index]);
+      } else {
+        break;
+      }
+    }
+  }
+
+  resultPagination.totalPage = totalPage;
+  resultPagination.currentPage = currentPage;
+  resultPagination.data = data;
+  res.send({
+    totalPage: resultPagination.totalPage,
+    resultPagnition: resultPagination.data,
+    currentPage: currentPage,
+  });
 });
 
 function _calculateAge(birthday) {
-    // birthday is a date
-    var ageDifMs = Date.now() - birthday.getTime();
-    var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // birthday is a date
+  var ageDifMs = Date.now() - birthday.getTime();
+  var ageDate = new Date(ageDifMs); // miliseconds from epoch
+  return Math.abs(ageDate.getUTCFullYear() - 1970);
 }
-user.get("/list/ajax", async(req, res) => {
-    const users = await userModel.list();
+user.get("/list/ajax", async (req, res) => {
+  const users = await userModel.list();
 
-    users.forEach((element) => {
-        element.Tuoi = _calculateAge(element.NgaySinh);
-    });
+  users.forEach((element) => {
+    element.Tuoi = _calculateAge(element.NgaySinh);
+  });
 
-    res.send(users);
+  res.send(users);
 });
 
-user.get("/detail", async(req, res) => {
+user.get("/detail", async (req, res) => {
+  const MaNLQ = req.query.id;
+  const detailInfo = await userModel.detail(MaNLQ);
 
-    const MaNLQ = req.query.id;
-    const detailInfo = await userModel.detail(MaNLQ);
+  var options = { day: "2-digit", month: "2-digit", year: "numeric" };
+  detailInfo.detail.NgaySinh = detailInfo.detail.NgaySinh.toLocaleDateString(
+    "vi-VN",
+    options
+  );
 
-    var options = { day: "2-digit", month: "2-digit", year: "numeric" };
-    detailInfo.detail.NgaySinh = detailInfo.detail.NgaySinh.toLocaleDateString(
-        "vi-VN",
-        options
-    );
-
-    res.render("manager/user/detail", {
-        userDetail: detailInfo.detail,
-        relatedUsers: detailInfo.DSNguoiLienDoi,
-        path: "/manager/user/detail",
-    });
+  res.render("manager/user/detail", {
+    userDetail: detailInfo.detail,
+    relatedUsers: detailInfo.DSNguoiLienDoi,
+    path: "/manager/user/detail",
+  });
 });
 
-user.post("/update", async(req, res) => {
-    const userUpdated = req.body;
-    try {
-        const entity = {
-            TrangThaiHienTai: userUpdated.trangthaimoi,
-            NoiDieuTri: userUpdated.noidieutrimoi,
-        };
-        const result = await userModel.update(entity, userUpdated.id);
-        return res.redirect(`/manager/user/detail?id=${userUpdated.id}`);
-    } catch (error) {
-        return res.redirect(`/manager/user/detail?id=${userUpdated.id}`);
+user.post("/update", async (req, res) => {
+  const userUpdated = req.body;
+  try {
+    const entity = {
+      TrangThaiHienTai: userUpdated.trangthaimoi,
+      NoiDieuTri: userUpdated.noidieutrimoi,
+    };
+    const result = await userModel.update(entity, userUpdated.id);
+    return res.redirect(`/manager/user/detail?id=${userUpdated.id}`);
+  } catch (error) {
+    return res.redirect(`/manager/user/detail?id=${userUpdated.id}`);
+  }
+});
+
+user.get("/create", async (req, res) => {
+  if (req.cookies("createUser")) {
+    message = req.cookies("create");
+  }
+  return res.render("manager/userCreate", {
+    msg: message,
+  });
+});
+
+user.post("/create", async (req, res) => {
+  res.clearCookie("createUser");
+  try {
+    var DOB = "";
+    if (req.body.ngaysinh) {
+      DOB = req.body.ngaysinh;
+      DOBReverse = req.body.ngaysinh.split("-").reverse().join("-");
     }
-});
 
-user.get("/create", async(req, res) => {
-    if (req.cookies("createUser")) {
-        message = req.cookies("create");
+    const entity = {
+      HoTen: req.body.ten,
+      CCCD: req.body.cccd,
+      SoDienThoai: req.body.sdt,
+      NgaySinh: DOBReverse,
+      DiaChi: req.body.diachi,
+      TrangThaiHienTai: req.body.trangthaihientai,
+      NoiDieuTri: req.body.noidieutri,
+    };
+
+    const username = req.body.cccd;
+    var password = "";
+    DOBArray = DOB.split("-");
+    for (var x in DOBArray) {
+      password += DOBArray[x];
     }
-    return res.render("manager/userCreate", {
-        msg: message,
-    });
-});
+    const passwordHashed = await bcrypt.hash(password, saltRound);
 
-user.post("/create", async(req, res) => {
-    res.clearCookie("createUser");
-    try {
-        var dob = "";
-        if (req.body.ngaysinh) {
-            dob = req.body.ngaysinh.split("-").reverse().join("-");
-        }
+    const accountInfo = {
+      Username: username,
+      Password: passwordHashed,
+      TrangThai: 0,
+    };
 
-        const entity = {
-            HoTen: req.body.ten,
-            CCCD: req.body.cccd,
-            SoDienThoai: req.body.sdt,
-            NgaySinh: dob,
-            DiaChi: req.body.diachi,
-            TrangThaiHienTai: req.body.trangthaihientai,
-            NoiDieuTri: req.body.noidieutri,
-        };
+    const result = await userModel.create(entity);
 
-        const result = await userModel.create(entity);
-        if (result) {
-            res.cookie("createUser", "Thêm người liên quan covid thành công.");
-        } else {
-            res.cookie("createUser", "Thêm người liên quan covid không thành công.");
-        }
-
-        res.send(result);
-
-    } catch (error) {
-        console.log("Error: " + error);
-        return res.redirect("/manager/user/list");
+    if (result) {
+      accountInfo.NguoiLienQuan = result.MaNguoiLienQuan;
+      const accountInserted = await userModel.createAccount(accountInfo);
+      if (accountInserted) {
+        res.cookie("createUser", "Thêm người liên quan covid thành công.");
+      } else {
+        res.cookie(
+          "createUser",
+          "Thêm người liên quan covid không thành công."
+        );
+      }
+    } else {
+      res.cookie("createUser", "Thêm người liên quan covid không thành công.");
     }
+
+    res.send(result);
+  } catch (error) {
+    console.log("Error: " + error);
+    return res.redirect("/manager/user/list");
+  }
 });
 user.post("/relation", async(req, res) => {
     res.clearCookie("createUser");
--- models/manager/user.model.js
+++ models/manager/user.model.js
@@ -1,47 +1,47 @@
 const db = require('../user');
 
-const tbUser = 'NguoiLienQuan';
-const tbHistoryManager = 'LichSuDuocQuanLy';
-const tbIsolationPlace = 'NoiDieuTriCachLy';
-const moiLienHe = 'MoiLienHe';
-
+const tbUser='NguoiLienQuan';
+const tbHistoryManager='LichSuDuocQuanLy';
+const tbIsolationPlace='NoiDieuTriCachLy';
+const tbAccountUser = "TaiKhoanNguoiDung";
 
 module.exports = {
-    list: async() => {
-        const res = await db.loadListUser();
-        return res;
-    },
-    addRelation: async(entity) => {
-        const res = await db.addRelation(entity);
-        return res;
-    },
-    detail: async(value) => {
-        const res = await db.viewDetailUser(value);
-        return res;
-    },
-    update: async(entity, value) => {
-        const res = await db.updateUser(entity, value);
-        return res;
-    },
+  list: async () => {
+    const res = await db.loadListUser();
+    return res;
+  },
+  detail: async (value) => {
+    const res = await db.viewDetailUser(value);
+    return res;
+  },
+  update: async (entity, value) => {
+    const res = await db.updateUser(entity, value);
+    return res;
+  },
 
-    create: async(entity) => {
-        const res = await db.createUser(entity);
-        return res;
-    },
+  create: async (entity) => {
+    const res = await db.createUser(entity);
+    return res;
+  },
 
-    get: async(value) => {
-        const res = await db.loadProfile(value, tbUser);
-        return res;
-    },
+  createAccount: async (entity) => {
+    const res = await db.createAccount(entity, tbAccountUser);
+    return res;
+  },
 
-    history: async(value) => {
-        const res = await db.loadHistory(value, tbHistoryManager);
-        return res;
-    },
+  get: async (value) => {
+    const res = await db.loadProfile(value, tbUser);
+    return res;
+  },
 
-    place: async(value) => {
-        const res = await db.loadIsoPlace(value, tbIsolationPlace);
-        return res;
-    },
+  history: async (value) => {
+    const res = await db.loadHistory(value, tbHistoryManager);
+    return res;
+  },
 
+  place: async (value) => {
+    const res = await db.loadIsoPlace(value, tbIsolationPlace);
+    return res;
+  },
 };
+
