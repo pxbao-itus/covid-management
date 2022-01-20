9cbb5875ab6527cb5f79c8aa1feea280410d40d5
XuanBao <pxbao.itus@gmail.com>
2022-01-20 21:55:40 +0700

    abc

--- public/js/user/package.detail.js
+++ public/js/user/package.detail.js
@@ -31,34 +31,41 @@ window.onload = async () => {
     $('#total').text(`Tổng tiền: ${formatCurrency(total)} đồng`);
 
     $('#buy').click( async (event) => {
-        const loan = await $.get('https://covid-management-21-22.herokuapp.com/api/get-loan');
-        const level = await $.get('https://covid-management-21-22.herokuapp.com/api/get-level');
-        if((total + parseInt(loan.SoDuNo)) <= parseInt(level.HanMuc)) {
-            const today = new Date();
-            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            package.package.ThoiGian = date + ' ' + time;
-            package.package.SoTien = total;
-            fetch('/order/buy', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(package),
+        try {
+            const loan = await $.get('https://covid-management-21-22.herokuapp.com/api/get-loan');
+            const level = await $.get('https://covid-management-21-22.herokuapp.com/api/get-level');
+            if((total + parseInt(loan.SoDuNo)) <= parseInt(level.HanMuc)) {
+                const today = new Date();
+                const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+                const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                package.package.ThoiGian = date + ' ' + time;
+                package.package.SoTien = total;
+                fetch('/order/buy', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(package),
+                    })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
                 })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-            $(this).trigger('click');
-        } else {
-            event.preventDefault();
-            alert("Vượt quá hạn mức tối thiểu");
-            return false;
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+                $(this).trigger('click');
+            } else {
+                event.preventDefault();
+                alert("Vượt quá hạn mức tối thiểu");
+                return false;
+            }
+        } catch (error) {
+            console.log(error);
+            alert('Mua gói nhu yếu phẩm không thành công!');
+            e.preventDefault();
         }
+        
         
     })
 
