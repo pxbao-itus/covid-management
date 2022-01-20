a2aa8b979eec3753cb0062037d489bb2fea6cd40
anhtoan1103 <you@example.com>
2022-01-20 17:13:19 +0700

    change http to https

--- public/js/list.js
+++ public/js/list.js
@@ -1,12 +1,12 @@
 function hover(element) {
-  element.setAttribute("src", "http://dummyimage.com/100x100/eb00eb/fff");
+    element.setAttribute("src", "https://dummyimage.com/100x100/eb00eb/fff");
 }
 
 function unhover(element) {
-  element.setAttribute("src", "http://dummyimage.com/100x100/000/fff");
+    element.setAttribute("src", "https://dummyimage.com/100x100/000/fff");
 }
 
-$(".item-img-container").hover(function () {
-  $(this).fadeOut(100);
-  console.log("dsffs");
+$(".item-img-container").hover(function() {
+    $(this).fadeOut(100);
+    console.log("dsffs");
 });
